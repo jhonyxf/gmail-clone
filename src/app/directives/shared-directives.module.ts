3aea0b06b5178ca510f8f8cde422229c4d3c9426
sidney_silva_verity <jhonyxf@gmail.com>
2021-08-05 09:15:01 -0300

    Criação de uma animação FAB durante a rolagem

--- src/app/directives/shared-directives.module.ts
+++ src/app/directives/shared-directives.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HideHeaderDirective } from './hide-header.directive';
+import { AnimatedFabDirective } from './animated-fab.directive';
 
 
 
 @NgModule({
   declarations: [
-    HideHeaderDirective
+    HideHeaderDirective,
+    AnimatedFabDirective
   ],
   imports: [
     CommonModule
   ],
-  exports: [HideHeaderDirective]
+  exports: [
+    HideHeaderDirective,
+    AnimatedFabDirective
+  ]
 })
 export class SharedDirectivesModule { }
